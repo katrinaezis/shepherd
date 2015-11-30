e1962ba455e3e1fac957e15019e5d54ec6d27e9b
Julian Bossiere <julianbossiere@gmail.com>
2015-11-30 13:37:50 -0800

    fixed changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var myApp = angular.module('myApp', ['ui.router']);
 
 myApp.config(function($stateProvider) {
@@ -28,7 +26,35 @@ myApp.config(function($stateProvider) {
 });
 
 myApp.controller('IntroController', function($scope) {
+	// tour.start();
+	var tour;
 
+	tour = new Shepherd.Tour({	
+	  defaults: {
+	    classes: 'shepherd-theme-arrows',
+	    scrollTo: true
+	  }
+	});
+
+	tour.addStep('example-step', {
+		title: 'Hi there!',
+		text: 'This would help you get started with this application!',
+		attachTo: '#shepherd-intro top',
+		classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+		buttons: [
+			{
+				text: 'Exit',
+				classes: 'shepherd-button-secondary',
+				action: function() {
+					return tour.hide();
+				}
+			}, {
+				text: 'Next',
+				action: tour.next,
+				classes: 'shepherd-button-example-primary'
+			}
+		]
+	});
 });
 
 myApp.controller('OneController', function($scope) {
@@ -36,44 +62,11 @@ myApp.controller('OneController', function($scope) {
 });
 
 myApp.controller('TwoController', function($scope) {
-	$scope.string = 'Hello';
 });
 
 myApp.controller('ThreeController', function($scope) {
-	tour.start();
 });
 
 
-var tour;
 
-tour = new Shepherd.Tour({	
-  defaults: {
-    classes: 'shepherd-theme-arrows',
-    scrollTo: true
-  }
-});
 
-console.log("sup");
-tour.addStep('example-step', {
-	title: 'Hi there!',
-	text: 'This would help you get started with this application!',
-	attachTo: '#hello bottom',
-	classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-	buttons: [
-		{
-			text: 'Exit',
-			classes: 'shepherd-button-secondary',
-			action: function() {
-				return tour.hide();
-			}
-		}, {
-			text: 'Next',
-			action: tour.next,
-			classes: 'shepherd-button-example-primary'
-		}
-	]
-});
-
-$(document).ready(function() {
-	tour.start();
-});
